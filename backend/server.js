e22f563736400b45a88797cd14655dd70318aa29
Vinay-Chinta <chintavinay895@gmail.com>
2026-04-27 11:41:35 +0530

    Add additional allowed origin for frontend

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const allowedOrigins = [
-  process.env.FRONTEND_URL || "http://localhost:5173",
+  process.env.FRONTEND_URL || "http://localhost:5173","http://localhost:5174",
   "https://pharma-navy-omega.vercel.app/",
 ];
 
