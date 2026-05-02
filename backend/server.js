b4bbb34f8da8e82f4d5b3af43d1afb1d2ed2b2ab
vinay1226n <chintavinay895@gmail.com>
2026-05-03 00:39:12 +0530

    fix cloudinary commonjs config

--- backend/server.js
+++ backend/server.js
@@ -67,4 +67,6 @@ Admin.findOne({ email: "admin@gmail.com" }).then((admin) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
